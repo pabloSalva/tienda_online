65c558a4c36e5eb8ac8d131a385a1eb93a44bb5e
pabloSalva <pablo.salvador90@gmail.com>
2020-03-15 16:04:02 -0300

    estilos en formulario

--- tienda_online/tienda_online/forms.py
+++ tienda_online/tienda_online/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(min_length=4, max_length=50, required=True)
-    email = forms.EmailField(required=True)
-    password = forms.CharField(min_length=8, max_length=50, required=True)
+    username = forms.CharField(min_length=4, max_length=50, required=True,
+                                widget=forms.TextInput(attrs={
+                                    'class' : 'form-control',
+                                    'id' : 'username',
+                                    'placeholder' : 'username'
+                                }))
+    email = forms.EmailField(required=True,
+                             widget=forms.EmailInput(attrs={
+                                 'class' : 'form-control',
+                                 'id' : 'email',
+                                 'placeholder'  : 'example@mail.com'
+                             }))
+    password = forms.CharField(min_length=8, max_length=50, required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'class' : 'form-control',
+                                    'id' : 'password',
+                                }))
